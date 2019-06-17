ebbaa9c6b43aca8594bd1e5cde0e6ea2e0cecb2f
lmsdkdev <devsdk@lemmatechnologies.com>
2019-06-17 08:16:12 +0530

    Added video interstitial screen & apis

---
+++ sampleapp/app/src/main/java/app/test/lm/lemmatestapp/VideoInterstitialActivity.java
@@ -0,0 +1,93 @@
+package app.test.lm.lemmatestapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+
+import lemma.lemmavideosdk.videointerstitial.LMVideoInterstitial;
+
+public class VideoInterstitialActivity extends Activity {
+
+    private static final String TAG = "InterstitialActivity";
+
+    private LMVideoInterstitial interstitial;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_interstitial_activity);
+
+        findViewById(R.id.load_ad).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.show_ad).setEnabled(false);
+                loadAd();
+
+            }
+        });
+
+        findViewById(R.id.show_ad).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != interstitial) {
+                    interstitial.show();
+                } else {
+                    Toast.makeText(VideoInterstitialActivity.this, "Please load Ad before showing it", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void initAd() {
+        if (null != interstitial) {
+            interstitial.destroy();
+            interstitial = null;
+        }
+
+        interstitial = new LMVideoInterstitial(this, "1", "1234", "http://demo1859652.mockable.io/videoad");
+        interstitial.setListener(new VideoInterstitialListener());
+    }
+
+    private void loadAd() {
+        initAd();
+        interstitial.loadAd();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != interstitial) {
+            interstitial.destroy();
+        }
+
+    }
+
+    class VideoInterstitialListener implements LMVideoInterstitial.LMVideoInterstitialListener {
+
+        @Override
+        public void onAdReceived(LMVideoInterstitial ad) {
+            findViewById(R.id.show_ad).setEnabled(true);
+        }
+
+        @Override
+        public void onAdFailed(LMVideoInterstitial ad, Error error) {
+            Log.e(TAG, "Ad failed with error - " + error.toString());
+        }
+
+        @Override
+        public void onAdOpened(LMVideoInterstitial ad) {
+            Log.d(TAG, "Ad Opened");
+        }
+
+        @Override
+        public void onAdClosed(LMVideoInterstitial ad) {
+            Log.d(TAG, "Ad Closed");
+            interstitial = null;
+        }
+
+    }
+}
